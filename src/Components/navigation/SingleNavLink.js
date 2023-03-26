d76c4291c60fc44cfd1c4d1e04574b61a2f17519
Harsh Tripathi <harshtripathistar@gmail.com>
2023-03-26 20:09:06 +0530

    header Complete

--- src/Components/navigation/SingleNavLink.js
+++ src/Components/navigation/SingleNavLink.js
@@ -6,31 +6,29 @@ const SingleNavLink = ({ categoryName, childOptions }) => {
   const [showChildLinks, setShowCHildLinks] = useState(false);
 
   return (
-    <div className="navLinks">
-      <ul>
-        <li className="headerLink">
-          <a href="http://">{categoryName}</a>
-          <button
-            className="expand"
-            onClick={() => setShowCHildLinks(!showChildLinks)}
-          >
-            <GoChevronDown id="down-icon" />
-          </button>
+    <ul className="headerLink">
+      <li className="haeding-Link">
+        <a href="http://">{categoryName}</a>
+        <button
+          className="expand"
+          onClick={() => setShowCHildLinks(!showChildLinks)}
+        >
+          <GoChevronDown id="down-icon" />
+        </button>
+      </li>
 
+      {showChildLinks && (
+        <ul>
           {childOptions.map((childCategory, index) => {
             return (
-              <ul>
-                {showChildLinks && (
-                  <li className="child-list">
-                    <a href="http://">{childCategory}</a>
-                  </li>
-                )}
-              </ul>
+              <li className="child-list">
+                <a href="http://">{childCategory}</a>
+              </li>
             );
           })}
-        </li>
-      </ul>
-    </div>
+        </ul>
+      )}
+    </ul>
   );
 };
 
