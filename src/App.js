f012d2f69bd7f2fc3aec0f1d8f23e191480d07cc
Harsh Tripathi <harshtripathistar@gmail.com>
2023-07-21 14:58:49 +0530

    updated app js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Footer from "./Components/Footer/Footer";
 
-
+import Header from "./Components/Header/Header";
 import Home from "./Pages/Home";
 
 function App() {
   return (
     <>
       <Header />
-
+      <Home />
       <div style={{ minHeight: "100vh" }}></div>
       <Footer />
     </>
