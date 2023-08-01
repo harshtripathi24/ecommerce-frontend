2a68bc44a6fee0973bebaf8ea176cccb63e562a2
Harsh Tripathi <harshtripathistar@gmail.com>
2023-08-01 15:36:57 +0530

    Navigation opening Design updated

--- src/Components/navigation/SingleNavLink.js
+++ src/Components/navigation/SingleNavLink.js
@@ -1,34 +1,52 @@
 import React, { useState } from "react";
 
 import { GoChevronDown } from "react-icons/go";
+import { BiChevronRight } from "react-icons/bi";
+import { FaAngleRight } from "react-icons/fa";
 
 const SingleNavLink = ({ categoryName, childOptions }) => {
   const [showChildLinks, setShowCHildLinks] = useState(false);
 
   return (
-    <ul className="headerLink">
-      <li className="haeding-Link">
-        <a href="http://">{categoryName}</a>
-        <button
-          className="expand"
-          onClick={() => setShowCHildLinks(!showChildLinks)}
-        >
-          <GoChevronDown id="down-icon" />
-        </button>
-      </li>
+    <>
+      <div className="NavLinkContainer">
+        <div className="headerLink">
+          <li className="haeding-Link">
+            <a
+              onMouseEnter={() => setShowCHildLinks(true)}
+              onMouseLeave={() => setShowCHildLinks(false)}
+              href="http://"
+            >
+              {categoryName}
+            </a>
+            <button
+              className="expandButton"
+              onMouseEnter={() => setShowCHildLinks(true)}
+              onMouseLeave={() => setShowCHildLinks(false)}
+              onClick={() => setShowCHildLinks(!showChildLinks)}
+            >
+              <FaAngleRight id="down-icon" />
+            </button>
+          </li>
+        </div>
 
-      {showChildLinks && (
-        <ul>
-          {childOptions.map((childCategory, index) => {
-            return (
-              <li className="child-list">
-                <a href="http://">{childCategory}</a>
-              </li>
-            );
-          })}
-        </ul>
-      )}
-    </ul>
+        {showChildLinks && (
+          <div
+            className="childLinkDiv"
+            onMouseEnter={() => setShowCHildLinks(true)}
+            onMouseLeave={() => setShowCHildLinks(false)}
+          >
+            {childOptions.map((childCategory, index) => {
+              return (
+                <div className="child-list">
+                  <a href="http://">{childCategory}</a>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
