305fdbd8c2d6365b08487c497da29970a9c8e0dd
Harsh Tripathi <harshtripathistar@gmail.com>
2024-04-02 13:13:15 +0530

    Searched Page v1 Done

--- src/Pages/SearchedPage.js
+++ src/Pages/SearchedPage.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { FiFilter } from "react-icons/fi";
 
 import { useGlobalContext } from "../Utilities/Context/Context";
 
+import Products from "../Components/Products/ProductData";
+
+import FilterModal from "../Utilities/Components/FilterModal";
 import "./SearchedPage.css";
 import ProductDevisonSection from "../Components/Products/ProductDevisonSection";
 const SearchedPage = () => {
+  const [searchedProduct, setSearchedProduct] = useState();
+  const [windoWidth, setWindoWidth] = useState(window.innerWidth);
+
   const { openModal, closeModal, openFilterModal, closeFilterModal } =
     useGlobalContext();
 
@@ -15,29 +21,76 @@ const SearchedPage = () => {
     openModal();
   };
 
-  return (
-    <>
-      <div className="SearchedPageMain">
-        <div className="filterDiv">
-          <button
-            className="filterBtn"
-            onClick={() => handleFilterBtn()}
-            type="button"
-          >
-            <FiFilter /> Filter
-          </button>
-        </div>
+  const detectWindowWidth = () => {
+    setWindoWidth(window.innerWidth);
+  };
 
-        <div className="Products">
-          <ProductDevisonSection
-            sectionType={2}
-            productIds={[]}
-            devisonTitle={"You Searched For"}
-          />
+  useEffect(() => {
+    const FetchProdctIds = async () => {
+      const prodIds = await Products.map((product) => product.pid);
+      setSearchedProduct(prodIds);
+    };
+
+    FetchProdctIds(); // run it, run it
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("resize", detectWindowWidth);
+
+    return () => {
+      window.removeEventListener("resize ", detectWindowWidth);
+    };
+  }, [windoWidth]);
+
+  if (windoWidth > 1232) {
+    return (
+      <>
+        <div className="SearchedPageMain">
+          <div className="deskFilterDiv">
+            <FilterModal />
+          </div>
+
+          <div className="Products">
+            {console.log("Searched Proudct now is " + searchedProduct)}
+            {searchedProduct && (
+              <ProductDevisonSection
+                sectionType={2}
+                productIds={searchedProduct}
+                devisonTitle={"You Searched For"}
+              />
+            )}
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="SearchedPageMain">
+          <div className="filterDiv">
+            <button
+              className="filterBtn"
+              onClick={() => handleFilterBtn()}
+              type="button"
+            >
+              <FiFilter /> Filter
+            </button>
+          </div>
+
+          <div className="Products">
+            {console.log("Searched Proudct now is " + searchedProduct)}
+            {searchedProduct && (
+              <ProductDevisonSection
+                sectionType={2}
+                productIds={searchedProduct}
+                devisonTitle={"You Searched For"}
+              />
+            )}
+          </div>
+        </div>
+      </>
+    );
+  }
 };
 
 export default SearchedPage;
